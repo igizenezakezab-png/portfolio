a4c6a6cea38704dce90eb41ab9567f2f596a0164
12keza <igizenezakezab@gmail.com>
2026-04-15 22:42:20 -0700

    web done

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -39,7 +39,7 @@ export default function Projects() {
 
           <button
             style={styles.button}
-            onClick={() => openProject("https://todo-app-xntu.vercel.app/")}
+            onClick={() => openProject("https://todo-app-xntu.vercel.app//")}
           >
             TODO APP
           </button>
